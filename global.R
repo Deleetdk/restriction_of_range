7c4f6159092109663d5fa0118e898fe1726fbdf8
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-04 14:00:14 +0200

    rewrote load libs

--- global.R
+++ global.R
@@ -1,7 +1,10 @@
 #global.R
 
-library(pacman)
-p_load(ggplot2, devtools, stringr)
+#not using pacman
+library(ggplot2)
+library(devtools)
+library(stringr)
 
+#load data
 data = read.csv("data.csv",row.names = 1) #load data
 title = "Understanding restriction of range"
