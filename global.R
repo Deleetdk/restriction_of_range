599fa8dd1edaa326cea43d512081e2a948992ef9
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-04 13:58:40 +0200

    removed grid package load

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 #global.R
 
 library(pacman)
-p_load(ggplot2, devtools, grid, stringr)
+p_load(ggplot2, devtools, stringr)
 
 data = read.csv("data.csv",row.names = 1) #load data
 title = "Understanding restriction of range"
