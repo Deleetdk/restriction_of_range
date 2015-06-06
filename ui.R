ff54f6e457a7204896fd0978de91d96fca02fb27
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-07 00:55:38 +0200

    improved presentation, added made by notice

--- ui.R
+++ ui.R
@@ -17,14 +17,16 @@ shinyUI(fluidPage(
     
     mainPanel(
       #The text with statistical information about the results.
-      p("Studies of cognitive ability often use samples that are not representative of the population. This is especially salient if the subjects are university students, a highly selected group."),
-      p("This practice means that correlations found in studies using students are too low if they are meant as an estimate of the effect size in the total population."),
-      p("Fortunately, methods exist that can correct for this bias, see ", em("Methods of Meta-analysis"), " by Frank Schmidt and John Hunter."),
-      p("The figure below visualizes the effect by color-coding the excluded datapoints and showing the correlation in the sample."),
+      h3("Explanation"),
+      p("Studies of cognitive ability often use samples that are not representative of the population. This is especially salient if the subjects are university students, a highly selected group.",
+        "This practice means that correlations found in studies using students are too low if they are meant as an estimate of the effect size in the total population.",
+      "Fortunately, methods exist that can correct for this bias, see ", a(href = "https://www.goodreads.com/book/show/895784.Methods_of_Meta_Analysis", em("Methods of Meta-analysis")), " by Frank Schmidt and John Hunter.",
+      "The figure below visualizes the effect by color-coding the excluded datapoints and showing the correlation in the sample."),
       
       #the plot
-      plotOutput("plot"), width=8
-      
+      plotOutput("plot"),
+      p("Made by ", a(href="http://emilkirkegaard.dk", "Emil O. W. Kirkegaard"), " using ", a(href = "http://shiny.rstudio.com/", "Shiny for R"),
+        ". Source code available ", a(href = "https://github.com/Deleetdk/restriction_of_range", "here"), ".")
       )
   )
 ))
