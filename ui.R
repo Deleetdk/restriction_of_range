f7d26bde0fe3a107fc131ff54701bad7fce0a78c
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-12 04:55:33 +0200

    text changes

--- ui.R
+++ ui.R
@@ -17,7 +17,6 @@ shinyUI(fluidPage(
     
     mainPanel(
       #The text with statistical information about the results.
-      h3("Explanation"),
       p("Studies of cognitive ability often use samples that are not representative of the population. This is especially salient if the subjects are university students, a highly selected group.",
         "This practice means that correlations found in studies using students are too low if they are meant as an estimate of the effect size in the total population.",
       "Fortunately, methods exist that can correct for this bias, see ", a(href = "https://www.goodreads.com/book/show/895784.Methods_of_Meta_Analysis", em("Methods of Meta-analysis")), " by Frank Schmidt and John Hunter.",
@@ -25,8 +24,7 @@ shinyUI(fluidPage(
       
       #the plot
       plotOutput("plot"),
-      p("Made by ", a(href="http://emilkirkegaard.dk", "Emil O. W. Kirkegaard"), " using ", a(href = "http://shiny.rstudio.com/", "Shiny for R"),
-        ". Source code available ", a(href = "https://github.com/Deleetdk/restriction_of_range", "here"), ".")
+      HTML("Made by <a href='http://emilkirkegaard.dk'>Emil O. W. Kirkegaard</a> using <a href='http://shiny.rstudio.com/'/>Shiny</a> for <a href='http://en.wikipedia.org/wiki/R_%28programming_language%29'>R</a>. Source code available <a href='https://github.com/Deleetdk/restriction_of_range'>on Github</a>.")
       )
   )
 ))
